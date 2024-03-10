bc5e277c5fa898bde274f95ee23b2f08acf6ed59
ravi-kothapally <ravikothapalli90@gmail.com>
2024-03-10 09:42:12 +0530

    leetcode contest 388

--- src/main/java/com/dsa/Trees/Same_Tree_100.java
+++ src/main/java/com/dsa/Trees/Same_Tree_100.java
@@ -1,5 +1,8 @@
 package com.dsa.Trees;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /*
 Given the roots of two binary trees p and q, write a function to check if they are the same or not.Two binary trees are considered the same if they are structurally identical, and the nodes have the same value. Example 1:
 Input: p = [1,2,3], q = [1,2,3]
@@ -14,12 +17,15 @@ Output: false
 -104 <= Node.val <= 104
  */
 public class Same_Tree_100 {
+
   public boolean isSameTree(TreeNode p, TreeNode q) {
-    if(p==null && q==null)
+    if (p == null && q == null) {
       return true;
-    if (p == null || q == null || p.val != q.val)
+    }
+    if (p == null || q == null || p.val != q.val) {
       return false;
-    return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
   }
 
 }
